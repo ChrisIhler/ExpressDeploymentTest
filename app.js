7497d15e23f624c89bbf618aa16aa71ce1d1f86d
ChrisIhler <Ihlerconsulting@gmail.com>
2019-11-27 16:39:48 -0800

    express server working locally

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = ( process.env.PORT || 5000 )
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.disable('x-powered-by')
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').load()
+// }
+
+
+
+
+
+app.use((err, req, res, next) => {
+  console.log(err)
+  const status = err.status || 500
+  res.status(status).json({
+    error: err,
+    message: 'app.js'
+  })
+})
+
+app.use((req, res, next) => {
+  console.log(req)
+  res.status(404).json({
+    error: {
+      message: 'Error not found in app.js'
+    }
+  })
+})
+
+const listener = () => console.log(`Listening on port ${port}!`)
+app.listen(port, listener)
